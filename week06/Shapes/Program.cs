1a1de936c87952a10f8d3d3ae316552cddd0b0d5
Nick2590 <pacegoodsell@gmail.com>
2025-12-03 12:02:02 -0700

    shapes project added

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test single square
+        Square testSquare = new Square("Red", 5);
+        Console.WriteLine($"Square Color: {testSquare.GetColor()}");
+        Console.WriteLine($"Square Area: {testSquare.GetArea()}");
+
+        // Create List of Shapes (polymorphism)
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 4));
+        shapes.Add(new Rectangle("Green", 5, 10));
+        shapes.Add(new Circle("Yellow", 3));
+
+        Console.WriteLine("\n--- Shape List Output ---");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
